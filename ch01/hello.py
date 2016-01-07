cb1d23351410d7f37682826262d24b2cc0e671a4
Young Han Lee <cpumaker@gmail.com>
2016-01-07 17:42:51 +0900

    mod

--- ch01/hello.py
+++ ch01/hello.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-
-	return "<h1>Hello World!</h>"
+	if request.method == 'POST':
+		return json.dumps(request.form)
+	else:
+		return "<h1>Hello World!</h>"
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
